2c9998902fa460fded890b37f9384dc039025181
Khanhbroo <khanhmathias532211@gmail.com>
2025-06-29 11:02:37 +0700

    feat: Configuring MobileNavigation

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
 
-const MobileNavigation = () => {
-  return <div>MobileNavigation</div>;
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+
+import Image from "next/image";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+const MobileNavigation = ({ avatar }: { avatar: string }) => {
+  const [open, setOpen] = useState(false);
+  const pathName = usePathname();
+
+  return (
+    <header className="mobile-header">
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="logo"
+        width={120}
+        height={52}
+        className="h-auto"
+      />
+
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="Search"
+            width={30}
+            height={30}
+          />
+        </SheetTrigger>
+        <SheetContent className="shad-sheet h-screen px-3">
+          <div className="header-user">
+            <Image
+              src={avatar}
+              alt="avatar"
+              width={44}
+              height={44}
+              className="header-user-avatar"
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 };
 
 export default MobileNavigation;
